70519c341563806af1851b32fa13b55b45e49c9e
Karim Osama <karimbiscatcho@gmail.com>
2023-02-24 08:49:43 +0200

    add some packages

--- resources/views/layouts/vertical/app-sidebar.blade.php
+++ resources/views/layouts/vertical/app-sidebar.blade.php
@@ -32,7 +32,8 @@
 								<li><a href="{{url('element-width')}}" class="slide-item"> Width</a></li>
 							</ul>
 						</li>
-						<li class="slide">
+
+						{{-- <li class="slide">
 							<a class="side-menu__item" data-bs-toggle="slide" href="javascript:void(0);">
 								<svg xmlns="http://www.w3.org/2000/svg"  class="side-menu__icon" width="24" height="24" viewBox="0 0 24 24"><path d="M20 17V7c0-2.168-3.663-4-8-4S4 4.832 4 7v10c0 2.168 3.663 4 8 4s8-1.832 8-4zM12 5c3.691 0 5.931 1.507 6 1.994C17.931 7.493 15.691 9 12 9S6.069 7.493 6 7.006C6.069 6.507 8.309 5 12 5zM6 9.607C7.479 10.454 9.637 11 12 11s4.521-.546 6-1.393v2.387c-.069.499-2.309 2.006-6 2.006s-5.931-1.507-6-2V9.607zM6 17v-2.393C7.479 15.454 9.637 16 12 16s4.521-.546 6-1.393v2.387c-.069.499-2.309 2.006-6 2.006s-5.931-1.507-6-2z"/></svg>
 							<span class="side-menu__label">Elements</span><i class="angle fe fe-chevron-{{ app()->getLocale() == 'ar' ? 'left' : 'right' }}"></i></a>
@@ -356,7 +357,7 @@
                                  <li><a href="{{url('checkout')}}" class="slide-item"> Checkout</a></li>
 								<li><a href="{{url('wishlist')}}" class="slide-item">wishlist</a></li>
 							</ul>
-						</li>
+						</li> --}}
 					</ul>
 				</aside>
 				<!--aside closed-->
